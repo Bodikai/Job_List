149b7f334d0c31d829eaaa647bc99762fdb13f23
Bodikai <omar.salleh@gmail.com>
2016-11-25 16:26:54 +0000

    @jobs now uses HASH instead of ARRAY and stores dependencies

--- lib/joblist.rb
+++ lib/joblist.rb
@@ -1,30 +1,31 @@
 class JobList
 
   def initialize
-    @jobs = ""
+    @jobs = Hash.new
   end
 
   def add(input)
     if input != ""
-      @jobs << (@jobs == "" ? input : ',' + input)
+      @jobs = parse(input)
     end
+    show
+  end
+
+  def parse(str)
+    str.split(',').each { |j| @jobs[j.chr] = j.slice(1,j.length) }
     @jobs
   end
 
-  def check_format
-    # Check that input string is formatted correctly
-  end
-
-  def sort(s)
-    # Split incoming string (ie. newly added jobs)
-  end
-
-  def to_array(s)
-
-  end
-
   def show
-    # Show current jobs
+    @jobs.keys.join(',')
+  end
+
+  def prioritise
+    # PRIORITY 1 - Jobs with longest dependency chain
+    # PRIORITY 2 - Alphabetic order
+    # e.g. "b,c,e" (dependent) have priority over "a,f" (dependent),
+    #      d" and "g", resulting in "b,c,e,a,f,d,g"
+    @jobs.sort!
   end
 
 end
